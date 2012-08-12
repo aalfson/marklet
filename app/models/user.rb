b85763c294d48c2af84803c8b43b370a8dccd5f3
Aaron Alfson <aaron.alfson@gmail.com>
2012-08-12 14:11:03 -0400

    added uniqueness validation to user.name

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   has_many :bookmarks
+  
+  validates_presence_of :name
+  validates_uniqueness_of :name
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :id, :name
--- db/schema.rb
+++ db/schema.rb
@@ -35,10 +35,11 @@ ActiveRecord::Schema.define(:version => 20120812161705) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "name"
+    t.string   "name", :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["name"], :name => "index_users_on_name", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
