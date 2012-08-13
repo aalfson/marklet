51ba9041f24630f9bef564aaa044f0476b6e7f31
Aaron Alfson <aaron.alfson@gmail.com>
2012-08-12 20:45:30 -0400

    added functional test, test refactoring and clean up

--- test/functional/bookmark_controller_test.rb
+++ test/functional/bookmark_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class BookmarkControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should create bookmark" do
+    assert Bookmark.count == 0
+    post :post, url: "www.tripphix.com", title: "TripPhix", id: '1' 
+    assert Bookmark.count == 1
+  end
+
 end
