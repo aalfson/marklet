f446ecd31243c1a15d9ebbee065940f4232d82fa
Aaron Alfson <aaron.alfson@gmail.com>
2012-09-12 22:34:20 -0400

    category submit form check

--- public/marklet.js
+++ public/marklet.js
@@ -1,11 +1,48 @@
 function getMarkletForm() {
 	$(document).ready(function() {
 		
-		var form = document.createElement("div"); 
+		var form = document.createElement("form"); 
 		$(form).attr('id', 'markletOverlay'); 
-		$(form).attr('style', 'width: 50%; height: 50%; position: absolute; z-index: 1000; background-color: #000; margin: 0 auto 0 auto;');
+		$(form).attr('style', 'width: 500px; height: 200px; position: fixed; z-index: 10000; top: 100; left: 300; background-color: #fff; border-style: solid; border-width: 2px; border-radius: 4px; margin: 0 auto 0 auto; padding: 10px');
+		
+		var formTitle = document.createElement("h3"); 
+		$(formTitle).text("Marklet");
+		$(form).append(formTitle);
+		
+		var pageTitleFieldLabel = document.createElement("label"); 
+		$(pageTitleFieldLabel).attr("for", "markletPagetTitle");
+		$(pageTitleFieldLabel).text("Page Title:"); 
 
-		//var form = "<div id='markletOverlay' style='width: 100%; height: 100%; position: absolute; z-index: 1000; background-color: #000; ><h1>THIS IS ONE HELL OF AN OVERLAY!</h1></div>";
+		var pageTitleField = document.createElement("input"); 
+		$(pageTitleField).attr("id", "markletPageTitle"); 
+		$(pageTitleField).attr("type", "text");
+		
+		var pageCategoryLabel = document.createElement("label"); 
+		$(pageCategoryLabel).attr("for", "markletCategory");
+		$(pageCategoryLabel).text("Category:"); 
+
+		var pageCategoryField = document.createElement("input"); 
+		$(pageCategoryField).attr("id", "markletCategory"); 
+		$(pageCategoryField).attr("type", "select");
+
+		var cancel = document.createElement("input"); 
+		$(cancel).attr("id", "markletCancel"); 
+		$(cancel).attr("type", "button");
+		$(cancel).attr("value", "cancel");
+		
+		var submit = document.createElement("input"); 
+		$(submit).attr("name", "commit"); 
+		$(submit).attr("type", "submit");
+		$(submit).attr("value", "Submit"); 
+		
+		$(form).append(pageTitleFieldLabel); 
+		$(form).append(pageTitleField);
+		$(form).append(pageCategoryLabel); 
+		$(form).append(pageCategoryField);
+		$(form).append(document.createElement("br"));
+		$(form).append(cancel);
+		$(form).append(submit);
+		
 		$('body').prepend(form);
 	});
 }
