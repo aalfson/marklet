25d7029df1a6462ad574b5c68ed2f155ea33e32e
Aaron Alfson <aaron.alfson@gmail.com>
2012-09-13 22:28:59 -0400

    marklet.js refactoring

--- public/marklet.js
+++ public/marklet.js
@@ -1,47 +1,71 @@
+function createForm() {
+
+	var form = document.createElement("form"); 
+	$(form).attr('id', 'markletOverlay'); 
+	$(form).attr('style', 'width: 500px; height: 200px; position: fixed; z-index: 10000; top: 100; left: 300; background-color: #fff; border-style: solid; border-width: 2px; border-radius: 4px; margin: 0 auto 0 auto; padding: 10px');
+
+	return form;
+}
+
+function createFormTitle(form) {
+	
+	var formTitle = document.createElement("h3"); 
+	$(formTitle).text("Marklet");
+	$(form).append(formTitle);
+}
+
+function createTitleField(form) {
+	
+	var pageTitleFieldLabel = document.createElement("label"); 
+	$(pageTitleFieldLabel).attr("for", "markletPagetTitle");
+	$(pageTitleFieldLabel).text("Page Title:"); 
+
+	var pageTitleField = document.createElement("input"); 
+	$(pageTitleField).attr({"id": "markletPageTitle", "type": "text", "value": document.title});
+
+	$(form).append(pageTitleFieldLabel); 
+	$(form).append(pageTitleField);
+}
+
+function createCategorySelector(form) {
+	
+	var pageCategoryLabel = document.createElement("label"); 
+	$(pageCategoryLabel).attr("for", "markletCategory");
+	$(pageCategoryLabel).text("Category:"); 
+
+	var pageCategoryField = document.createElement("select"); 
+	$(pageCategoryField).attr("id", "markletCategory");
+	
+	$(form).append(document.createElement("br"));
+	$(form).append(pageCategoryLabel); 
+	$(form).append(pageCategoryField);
+}
+
+function createButtons(form) {
+	
+	var cancel = document.createElement("input"); 
+	$(cancel).attr("id", "markletCancel"); 
+	$(cancel).attr("type", "button");
+	$(cancel).attr("value", "Cancel");
+	
+	var submit = document.createElement("input"); 
+	$(submit).attr({"name": "commit", "type": "submit", "value": "Submit"}); 
+	
+	$(form).append(document.createElement("br"));
+	$(form).append(cancel);
+	$(form).append(submit);
+
+}
+
 function getMarkletForm() {
-	$(document).ready(function() {
-		
-		var form = document.createElement("form"); 
-		$(form).attr('id', 'markletOverlay'); 
-		$(form).attr('style', 'width: 500px; height: 200px; position: fixed; z-index: 10000; top: 100; left: 300; background-color: #fff; border-style: solid; border-width: 2px; border-radius: 4px; margin: 0 auto 0 auto; padding: 10px');
-		
-		var formTitle = document.createElement("h3"); 
-		$(formTitle).text("Marklet");
-		$(form).append(formTitle);
-		
-		var pageTitleFieldLabel = document.createElement("label"); 
-		$(pageTitleFieldLabel).attr("for", "markletPagetTitle");
-		$(pageTitleFieldLabel).text("Page Title:"); 
 
-		var pageTitleField = document.createElement("input"); 
-		$(pageTitleField).attr({"id": "markletPageTitle", "type": "text", "value": document.title}); 
-		
-		var pageCategoryLabel = document.createElement("label"); 
-		$(pageCategoryLabel).attr("for", "markletCategory");
-		$(pageCategoryLabel).text("Category:"); 
-
-		var pageCategoryField = document.createElement("select"); 
-		$(pageCategoryField).attr("id", "markletCategory"); 
-		// $(pageCategoryField).attr("multiple", "multiple");
-
-		var cancel = document.createElement("input"); 
-		$(cancel).attr("id", "markletCancel"); 
-		$(cancel).attr("type", "button");
-		$(cancel).attr("value", "Cancel");
-		
-		var submit = document.createElement("input"); 
-		$(submit).attr({"name": "commit", "type": "submit", "value": "Submit"}); 
-		
-		$(form).append(pageTitleFieldLabel); 
-		$(form).append(pageTitleField);
-		$(form).append(pageCategoryLabel); 
-		$(form).append(pageCategoryField);
-		$(form).append(document.createElement("br"));
-		$(form).append(cancel);
-		$(form).append(submit);
-		
-		$('body').prepend(form);
-	});
+	var form = createForm(); 
+	createFormTitle(form);
+	createTitleField(form); 
+	createCategorySelector(form); 
+	createButtons(form)
+	
+	$('body').prepend(form);
 }
 
 
@@ -55,14 +79,16 @@ function postBookmark() {
 		  type: 'POST',
 		  url: 'http://localhost:3000/bookmark/create',
 		  data: { url: loc, title: titleVal, id: markletid }, 
-		  dataType: "jsonp",
-		  //success: alert("Saved to marklet!"),
+		  dataType: "jsonp"
 		});		
 	});
 }
 
-function loadScript(url, callback)
-{
+/* 	The functions below are used to load jQuery and were slightly modified by me but essentially
+ 	written by someone else. Unfortunately, I have no idea where I got it from, 
+	but I want to give credit where it is due. Thanks! */
+	
+function loadScript(url, callback) {
         var head = document.getElementsByTagName("head")[0];
         var script = document.createElement("script");
         script.src = url;
@@ -100,7 +126,8 @@ function init() {
 	catch(err) {
 		if (typeof jQuery != 'function' || typeof $ != 'function') {
 			loadScript("http://localhost:3000/jquery-1.7.2.min.js", function() {
-				postBookmark();
+				// postBookmark();
+				getMarkletForm();
 			});	
 		}
 		else {
